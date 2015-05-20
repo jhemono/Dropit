56f3bd8297eb48d7bdb45ecfba679b8da8824762
Julien Hémono <jhemono@me.com>
2015-05-20 22:43:38 +0200

    Initial Commit

---
+++ Dropit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dropit
+//
+//  Created by Julien Hémono on 20/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
